b5681f0758718600c80472291bf1f0a6da9c42c3
Peter <peter.karas.mail@gmail.com>
2023-10-09 22:15:45 +0200

    final commit? resolve backend dockerization

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use an official Node.js runtime as a parent image
-FROM node:14
+FROM node:16
 
 # Set the working directory in the container
 WORKDIR /app
@@ -7,13 +7,15 @@ WORKDIR /app
 # Copy package.json and package-lock.json to the container
 COPY package*.json ./
 
-# Install Nest.js CLI globally
+# Install Nest.js CLI globally (this might not be necessary unless you're using the CLI in your Docker container)
 RUN npm install -g @nestjs/cli
 
+
 # Install application dependencies
-RUN npm install
 RUN npm i bcrypt
 RUN npm i crypto
+
+RUN npm i --save-dev @types/node
 
 # Copy the rest of the application code to the container
 COPY . .
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+
+const crypto = require('crypto');
 
 @Injectable()
 export class UsersService {
@@ -10,8 +12,30 @@ export class UsersService {
   ) {
   }
 
-  async cypher(): Promise<User[]> {
+  async cypher(): Promise<{encryptedData: string, iv: string}[]> {
     const users: User[] = await this.usersRepository.find();
-    return users;
+    const symmetricKey = this.generateSymmetricKey();
+
+    return users.map((user) => {
+      const dataToEncrypt = user.firstName + ' ' + user.lastName + ' ' + user.text;
+      const algorithm = 'aes-256-cbc';
+
+      // Generate a random IV
+      const iv = crypto.randomBytes(16);
+
+      // Encrypting the data
+      const cipher = crypto.createCipheriv(algorithm, symmetricKey, iv);
+      let encryptedData = cipher.update(dataToEncrypt, 'utf8', 'hex');
+      encryptedData += cipher.final('hex');
+
+      return {
+        encryptedData,
+        iv: iv.toString('hex') // Storing the IV as a hex string for easy transmission or storage
+      };
+    });
   }
+
+  private generateSymmetricKey = () => {
+    return crypto.randomBytes(32); // 32 bytový kľúč (256 bitov)
+  };
 }
