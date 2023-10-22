f2a09fbf685f0820188a51c730e8fd7ccc4fe86e
Karas Patrik <patrik.karas@extern.a1.at>
2023-10-22 17:08:48 +0200

    fixed bugs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -39,11 +39,11 @@ export class UsersService {
   }
 
   encryptSymmetricKeyWithPrivateKey(privateKey: string, symmetricKey: Buffer): string {
-    return crypto.privateEncrypt(privateKey, symmetricKey).toString('base64');
+    return crypto.publicEncrypt(privateKey, symmetricKey).toString('base64');
   }
 
   decryptSymmetricKeyWithPublicKey(encryptedKey: string, publicKey: string): Buffer {
-    return crypto.publicDecrypt(publicKey, Buffer.from(encryptedKey, 'base64'));
+    return crypto.privateDecrypt(publicKey, Buffer.from(encryptedKey, 'base64'));
   }
 
   generateKeyPair(): { publicKey: string; privateKey: string } {
@@ -64,7 +64,7 @@ export class UsersService {
   async cypher(): Promise<any> {
     const symmetricKey = crypto.randomBytes(32);
     const { publicKey, privateKey } = this.generateKeyPair();
-    const encryptedSymmetricKey = this.encryptSymmetricKeyWithPrivateKey(privateKey, symmetricKey);
+    const encryptedSymmetricKey = this.encryptSymmetricKeyWithPrivateKey(publicKey, symmetricKey); // encrypt the symmetric key with the public key
     const users = await this.usersRepository.find(); // find all users
 
     const dataToEncrypt = users.map((user) => { // map users to the data we want to encrypt
@@ -81,7 +81,7 @@ export class UsersService {
     await this.writeDataToJson(encryptedDataSet, FILENAME); // write the encrypted data to a file
     const encryptedDataFromFile = await this.readJsonFile(FILENAME); // read the encrypted data from the file
 
-    const decryptedSymmetricKey = this.decryptSymmetricKeyWithPublicKey(encryptedSymmetricKey, publicKey); // decrypt the symmetric key with the public key
+    const decryptedSymmetricKey = this.decryptSymmetricKeyWithPublicKey(encryptedSymmetricKey, privateKey); // decrypt the symmetric key with the private key
     if (decryptedSymmetricKey.toString() !== symmetricKey.toString()) { // check if the decrypted symmetric key matches the original one
       throw new Error('Symmetric key decryption failed!');
     }
